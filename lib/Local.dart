6875a41ddcf3138c56e886743879e6d85b5ba96c
mounikagajjala1999 <mounikagajjala1999@gmail.com>
2023-01-05 18:58:07 +0530

    new in marathi half

---
+++ lib/Local.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+class LocaleString extends Translations{
+  @override
+
+  Map<String, Map<String, String>> get keys => {
+    'en_US': {
+      'title': 'calender',
+    },
+    'mr_IN':{
+      'title':'कॅलेंडर',
+    },
+  };
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'Local.dart';
 
 void main() {
-  runApp(const MyApp());
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,12 +15,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'calender',
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      // title:'title'.tr,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Calender'),
+      home: MyHomePage(title: ''.tr),
     );
   }
 }
@@ -31,174 +36,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  List<String> values = [
-    "sun"
-        "mon"
-        "tue"
-        "wed"
-        "thu"
-        "fri"
-        "sat"
-        "1"
-        "1"
-        "2"
-        "3"
-        "4"
-        "5"
-        "6"
-        "7"
-        "2"
-        "8"
-        "9"
-        "10"
-        "11"
-        "12"
-        "13"
-        "14"
-        "3"
-        "15"
-        "16"
-        "17"
-        "18"
-        "19"
-        "20"
-        "21"
-        "4"
-        "22"
-        "23"
-        "24"
-        "25"
-        "26"
-        "27"
-        "28"
-        "5"
-        "29"
-        "30"
-        "31"
-  ];
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  @override
+  void initState() {
+    super.initState();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    _selectedDay = _focusedDay;
+    // _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: const Padding(
+          padding:  EdgeInsets.only(left:14,top: 18),
+          child:  Text('कॅलेंडर',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
         ),
-        body: Container(
-          height: 800,
-          width: 390,
-          // color: Colors.blue,
+        // centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              CarouselSlider(
-                options: CarouselOptions(height: 450.0),
-                items: [1, 2, 3, 4, 5,6,7,8,9,10,11,12].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration: const BoxDecoration(color: Colors.blue),
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    right: 230, top: 10, left: 10),
-                                child: Text(
-                                  'text $i',
-                                  style: const TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
+              Card(
+                margin: EdgeInsets.all(8.0),
+                elevation: 5.0,
+                shape: const RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.grey, width: 2.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: TableCalendar(
+                  daysOfWeekStyle: DaysOfWeekStyle(
+                      decoration: BoxDecoration(color: Colors.grey[200])),
 
-                            ],
-                          )
-
-                          // Text('text $i', style: const TextStyle(fontSize: 16.0),)
-                          );
-                    },
-                  );
-                }).toList(),
-              )
-              // Padding(
-              //   padding: EdgeInsets.only(left:0, top: 10, bottom: 10,right: 250),
-              //   child: Text("jan",
-              //       style: TextStyle(
-              //           fontSize: 25,
-              //           fontWeight: FontWeight.bold,
-              //           color: Colors.grey,),textAlign: TextAlign.left),
-              // ),
+                  calendarStyle: CalendarStyle(
+                      weekendDecoration: BoxDecoration(color: Colors.grey[100])),
+                  locale: 'mr_IN',
+                  focusedDay: DateTime.now(),
+                  firstDay: DateTime.utc(1999, 1, 1),
+                  lastDay: DateTime.utc(2030, 12, 31),
+                  calendarFormat: CalendarFormat.month,
+                  weekendDays: const [DateTime.sunday, 7],
+                  startingDayOfWeek: StartingDayOfWeek.monday,
+                  daysOfWeekHeight: 40.0,
+                  rowHeight: 60.0,
+                  selectedDayPredicate: (day) {
+                    return isSameDay(_selectedDay, day);
+                  },
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay =
+                          focusedDay; // update `_focusedDay` here as well
+                    });
+                  },
+                  onFormatChanged: (format) {
+                    if(_calendarFormat != format){
+                      setState(() {
+                        _calendarFormat = format;
+                      });
+                    }
+                  },
+                  onPageChanged: (focusedDay) {
+                    _focusedDay = focusedDay;
+                  },
+                ),
+              ),
             ],
           ),
-        )
-        // This trailing comma makes auto-formatting nicer for build methods.
-        );
-  }
-
-  Widget _buttons() {
-    return GridView.count(
-      mainAxisSpacing: 2,
-      // crossAxisSpacing: 3,
-      crossAxisCount: 7,
-      shrinkWrap: true,
-      children: List.generate(values.length, (index) {
-        return _buttonWidget(number: values[index]);
-      }),
-    );
-  }
-  Widget _monthsName() {
-   if(values == 0 ){
-     print("jan");
-   }
-   return Text("jan");
-
-  }
-
-  Widget _buttonWidget({number}) {
-    return InkWell(
-      onTap: () {
-        print(number);
-
-        switch (number) {
-          case "C":
-            break;
-          case "+":
-            break;
-          case "-":
-            break;
-          case "*":
-            break;
-          case "=":
-            break;
-          case "/":
-            break;
-
-          default:
-            "shut up";
-        }
-      },
-      child: Container(
-          alignment: Alignment.center,
-          color: Colors.black,
-          child: Text(number,
-              style: const TextStyle(fontSize: 50, color: Colors.deepOrange),
-              textAlign: TextAlign.center)),
+        ),
+      ),
     );
   }
 }
-// Padding(
-// padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
-// child: Text("jan",
-// style: TextStyle(
-// fontSize: 20,
-// fontWeight: FontWeight.bold,
-// color: Colors.grey)),
-// ),
